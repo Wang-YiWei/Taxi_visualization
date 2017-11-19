44e6aa32b75203cb7e924969374514bcfa061992
joanne <jallgs0502@gmail.com>
2017-11-20 04:38:52 +0800

    Add files via upload
    report

---
+++ taxi/javascript/empty_rate.js
@@ -0,0 +1,225 @@
+var rate_margin = { top: 20, right: 20, bottom: 70, left: 40 },
+    rate_width = screen.availWidth*0.4 - rate_margin.left - rate_margin.right,
+    rate_height =  screen.availWidth*0.3 - rate_margin.top - rate_margin.bottom,
+    rate_radius = Math.min(rate_width, rate_height) / 2;
+
+var rate_svg = d3.select("#img3").append("svg")
+    .attr("width", (rate_width + rate_margin.left + rate_margin.right))
+    .attr("height", (rate_height + rate_margin.top + rate_margin.bottom))
+    .append("g")
+    .attr("class", "rate_svg")
+    .attr("transdorm", "translate(" + rate_margin.left + "," + rate_margin.top + ")");
+
+rate_svg.append("g")
+    .append('text')
+    .attr("transform", "translate(100,0)")
+    .attr("dy", "1.20em")
+    .attr("font-size", "1.0em")
+    .style("text-anchor", "middle")
+    .style("fill", "black")
+    .text("計程車司機空車率");
+
+var x = d3.scaleBand().rangeRound([0, rate_width/5]).padding(0.1),
+    y = d3.scaleLinear().rangeRound([rate_height, 0]);
+
+var rate_arc = d3.arc()
+    .outerRadius(rate_radius - 50)
+    .innerRadius(rate_radius - 100);
+
+var pie = d3.pie()
+    .sort(null)
+    .value(function(d) { return d.value; })
+
+var rate_color = d3.scaleLinear()
+    .range(["yellow", "red"]);
+d3.csv("csvData/0703empty.csv", function(error, data) {
+    var count_rate = new Array(10);
+    var object_rate = [];
+    var object_rate_donut = [];
+    console.log(data);
+    count_rate[0] = 0
+    count_rate[1] = 0
+    count_rate[2] = 0
+    count_rate[3] = 0
+    count_rate[4] = 0
+    count_rate[5] = 0
+    count_rate[6] = 0
+    count_rate[7] = 0
+    count_rate[8] = 0
+    count_rate[9] = 0
+        //console.log(count_rate[6])
+    for (i = 0; i < data.length; i++) {
+        if (data[i].empty < 10) {
+            count_rate[0] == count_rate[0] + 1;
+        } else if (data[i].empty >= 10 && data[i].empty < 20) {
+            count_rate[1] == count_rate[1] + 1;
+        } else if (data[i].empty >= 20 && data[i].empty < 30) {
+            count_rate[2] = count_rate[2] + 1
+        } else if (data[i].empty >= 30 && data[i].empty < 40) {
+            count_rate[3] = count_rate[3] + 1
+        } else if (data[i].empty >= 40 && data[i].empty < 50) {
+            count_rate[4] = count_rate[4] + 1
+        } else if (data[i].empty >= 50 && data[i].empty < 60) {
+            count_rate[5] = count_rate[5] + 1
+        } else if (data[i].empty >= 60 && data[i].empty < 70) {
+            count_rate[6] = count_rate[6] + 1
+        } else if (data[i].empty >= 70 && data[i].empty < 80) {
+            count_rate[7] = count_rate[7] + 1
+        } else if (data[i].empty >= 80 && data[i].empty < 90) {
+            count_rate[8] = count_rate[8] + 1
+        } else if (data[i].empty >= 90 && data[i].empty < 100) {
+            count_rate[9] = count_rate[9] + 1
+        }
+    }
+    for (i = 0; i < count_rate.length; i++) {
+        object_rate.push({
+            key: i * 10,
+            value: +Math.floor(count_rate[i])
+        });
+    }
+    console.log(object_rate)
+    for (i = 0; i < object_rate.length; i++) {
+        object_rate_donut.push({
+            key: i * 10,
+            value: +(count_rate[i] / count_rate.length)
+        });
+    }
+    console.log(object_rate)
+        //console.log(object_rate.map(function (d) { return d.key }));
+    x.domain(object_rate.map(function(d, i) { return d.key }));
+    y.domain([0, d3.max(object_rate, function(d) { return d.value; })])
+    rate_color.domain([0, d3.max(object_rate, function(d, i) { return d.value; })]);
+
+    rate_svg.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(10," + rate_height + ")")
+        .call(d3.axisBottom(x));
+
+    rate_svg.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y))
+        .attr("transform", "translate(30,0)")
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("Frequency");
+    //console.log(x.bandwidth());
+    var rate_bar = rate_svg.selectAll(".bar")
+        .data(object_rate)
+        .enter().append('g');
+    rate_bar.append("rect")
+        .attr("opacity", 0.8)
+        .attr("x", function(d) { return x(d.key)+10; })
+        .attr("y", function(d) { return y(d.value)-0.5; })
+        .attr("width", x.bandwidth())
+        .attr("height", function(d) { return rate_height - y(d.value)+0.5 })
+        .attr("fill", function(d) { return rate_color(d.value) })
+        .on("mouseover", function(d) {
+            d3.select(this)
+                .attr("opacity", 1);
+        })
+        .on("mouseout", function(d) {
+            d3.select(this)
+                .attr("opacity", 0.8);
+        });
+    rate_bar.append("text")
+
+    var rate_donut = rate_svg.selectAll(".arc")
+        .data(pie(object_rate))
+        .enter().append("g");
+    var rate_donut_text = rate_donut.append("text")
+        .attr("transform", "translate(" + rate_width/1.2 + "," + rate_height / 2 + ")")
+        .attr("dy", "2.20em")
+        .attr("font-size", "1.0em")
+        .style("text-anchor", "middle")
+        .style("fill", "black")
+        .html("的司機" + "<tspan><br>");
+    var rate_donut_text1 = rate_donut.append("text")
+        .attr("transform", "translate(" + rate_width/1.2 + "," + rate_height / 2 + ")")
+        .attr("dy", "3.40em")
+        .attr("font-size", "1.0em")
+        .style("text-anchor", "middle")
+        .style("fill", "black")
+        .html("空車率在60%到70%之間");
+    var rate_donut_text2 = rate_donut.append("text")
+        .attr("transform", "translate(" + rate_width/1.2  + "," + rate_height / 2 + ")")
+        .attr("dy", ".00em")
+        .attr("font-size", "5em")
+        .style("text-anchor", "middle")
+        .style("fill", "black")
+        .text("49%");
+
+    var rate_donut_cir = rate_donut.append('circle')
+        .attr("transform", "translate(" + rate_width/1.2 + "," + rate_height / 2 + ")")
+        .attr("cx", "0")
+        .attr("cy", "0")
+        .attr("opacity", 0.1)
+        .attr("r", rate_radius - 105)
+        .style("fill", rate_color(401));
+
+    rate_donut.append("path")
+        .attr("transform", "translate(" + rate_width/1.2 + "," + rate_height / 2 + ")")
+        .attr("d", rate_arc)
+        .attr("opacity", 0.8)
+        .attr("stroke", "white")
+        .style("fill", function(d) { return rate_color(d.value) })
+        .on("mouseover", function(d) {
+            //console.log("111");
+            rate_donut_text
+            .text("");
+        rate_donut_text1
+            .text("")
+        rate_donut_text2
+            .text("")
+            console.log(data.length)
+            var temp_rate_arc = d3.arc()
+                .outerRadius(rate_radius - 50)
+                .innerRadius(rate_radius - 100);
+            var select_value = d3.select(this).data()[0].value;
+            var select_key = d3.select(this).data()[0].index;
+            console.log(d3.select(this).data()[0]);
+            console.log("sel"+select_value)
+            d3.select(this)
+                .attr("d", temp_rate_arc)
+                .attr("opacity", 0.8);
+            rate_donut_text.append("tspan")
+                .html("的司機" + "<tspan><br>");
+            rate_donut_text1.append("tspan")
+                .html("空車率在" + select_key * 10 + "%到" + (select_key + 1) * 10 + "%之間");
+            rate_donut_text2.append("tspan")
+                .html(count_rate_function(select_value) + "%");
+            rate_donut_cir
+                .attr("opacity", 0.1)
+                .style("fill", function(d) {
+                    return rate_color(select_value);
+                })
+        })
+        .on("mouseout", function(d) {
+            d3.select(this)
+                .attr("d", rate_arc)
+                .attr("opacity", 0.8);
+            rate_donut_cir
+                .attr("opacity", 0)
+                .style("fill", "white");
+            rate_donut_text
+                .text("");
+            rate_donut_text1
+                .text("")
+            rate_donut_text2
+                .text("")
+        })
+    var rate_donut_svg = rate_svg.selectAll(".rate_svg");
+    rate_svg.selectAll(".rate_svg .bar rect").attr("fill", "black").transition().duration(750);
+
+    function HTTPword(i, data) {
+        var tip = '';
+        tip = "有" + data + "的司機" + "<tspan><br>" + "空車率在" + i * 10 + "到" + (i + 1) * 10 + "之間";
+        return tip;
+    }
+
+    function count_rate_function(d) {
+        return Math.ceil(d * 100 / data.length);
+    }
+})
---
+++ taxi/javascript/money_day.js
@@ -0,0 +1,141 @@
+var money_margin = { top: 20, right: 20, bottom: 70, left: 40 },
+    money_width = screen.availWidth*0.25 - money_margin.left - money_margin.right,
+    money_height = screen.availWidth*0.3 - money_margin.top - money_margin.bottom,
+    money_radius = Math.min(money_width, money_height)/1.5;
+
+var money_svg = d3.select("#img4").append("svg")
+    .attr("width", (money_width + money_margin.left + money_margin.right) * 2)
+    .attr("height", (money_height + money_margin.top + money_margin.bottom))
+    .append("g")
+    .attr("class", "money_svg")
+    .attr("transdorm", "translate(" + money_margin.left + "," + money_margin.top + ")");
+
+money_svg.append("g")
+    .append('text')
+    .attr("transform", "translate(150,0)")
+    .attr("dy", "1.0em")
+    .attr("font-size", "1.0em")
+    .style("text-anchor", "middle")
+    .style("fill", "black")
+    .text("計程車司機每日平均所得");
+
+var x = d3.scaleBand().rangeRound([0, money_width]).padding(0.1),
+    y = d3.scaleLinear().rangeRound([money_height, 0]);
+
+var money_arc = d3.arc()
+    .outerRadius(money_radius - 30)
+    .innerRadius(money_radius - 100);
+
+var pie = d3.pie()
+    .sort(null)
+    .value(function(d) { return d.value; })
+
+var money_color = d3.scaleLinear()
+    .range(["#99FFFF", "blue"]);
+d3.csv("./csvData/money_day_date.csv", function(d) {
+    d.value = +d.value;
+    return d;
+}, function(error, data) {
+    x.domain(data.map(function(d, i) { return d.percent; }));
+    y.domain([0, d3.max(data, function(d, i) { return d.value; })])
+    money_color.domain([0, d3.max(data, function(d, i) { return d.value; })]);
+    console.log(data);
+    var money_donut = money_svg.selectAll(".arc")
+        .data(pie(data))
+        .enter().append("g");
+    console.log(pie(data));
+
+    var money_donut_text = money_donut.append("text")
+        .attr("transform", "translate(" + money_width + "," + money_height / 1.9 + ")")
+        .attr("dy", "2.60em")
+        .attr("font-size", "1.3em")
+        .style("text-anchor", "middle")
+        .style("fill", "black")
+        .html("的司機的每日營業額" + "<tspan><br>");
+    var money_donut_text1 = money_donut.append("text")
+        .attr("transform", "translate(" + money_width + "," + money_height / 1.9 + ")")
+        .attr("dy", "3.80em")
+        .attr("font-size", "1.3em")
+        .style("text-anchor", "middle")
+        .style("fill", "black")
+        .text("在 1000元到2000元 之間");
+
+    var money_donut_text2 = money_donut.append("text")
+        .attr("transform", "translate(" + money_width + "," + money_height / 1.9 + ")")
+        .attr("dy", ".00em")
+        .attr("font-size", "5em")
+        .style("text-anchor", "middle")
+        .style("fill", "black")
+        .text(" 53 %");
+    var money_donut_cir = money_donut.append('circle')
+        .attr("transform", "translate(" + money_width + "," + money_height / 1.9 + ")")
+        .attr("cx", "0")
+        .attr("cy", "0")
+        .attr("r", money_radius - 105)
+        .attr("opacity", 0.2)
+        .style("fill",  money_color(53));
+
+    money_donut.append("path")
+        .attr("transform", "translate(" + money_width + "," + money_height / 1.9 + ")")
+        .attr("d", money_arc)
+        .attr("opacity", 0.8)
+        .attr("stroke", "white")
+        .style("fill", function(d) { return money_color(d.value) })
+        .on("mouseover", function(d) {
+            //console.log("111");
+            var temp_money_arc = d3.arc()
+                .outerRadius(money_radius - 20)
+                .innerRadius(money_radius - 100);
+            var select_value = d3.select(this).data()[0].value;
+            var select_key = d3.select(this).data()[0].data.percent;
+            //console.log(d3.select(this).data()[0].data.percent);
+            d3.select(this)
+                .attr("d", temp_money_arc)
+                .attr("opacity", 0.8);
+            money_donut_text
+                .text("");
+            money_donut_text1
+                .text("")
+            money_donut_text2
+                .text("")
+            money_donut_text2.append("tspan")
+                .html(count_money_function(select_value) + "%");
+            money_donut_text.append("tspan")
+                .html("的司機的每日營業額" + "<tspan><br>");
+            money_donut_text1.append("tspan")
+                .html("在" + select_key + "之間");
+            console.log("select:"+select_value)
+
+            money_donut_cir
+                .attr("opacity", 0.2)
+                .style("fill", function(d) {
+                    return money_color(select_value);
+                })
+        })
+        .on("mouseout", function(d) {
+            d3.select(this)
+                .attr("d", money_arc)
+                .attr("opacity", 0.8);
+            money_donut_cir
+                .attr("opacity", 0)
+                .style("fill", "white");
+            money_donut_text
+                .text("");
+            money_donut_text1
+                .text("")
+            money_donut_text2
+                .text("")
+        })
+    var money_donut_svg = money_svg.selectAll(".money_svg");
+    money_svg.selectAll(".money_svg .bar rect").attr("fill", "black").transition().duration(750);
+
+    function HTTPword(i, data) {
+        var tip = '';
+        tip = "有" + data + "的司機" + "<tspan><br>" + "空車率在" + i * 10 + "到" + (i + 1) * 10 + "之間";
+        return tip;
+    }
+
+    function count_money_function(data) {
+        return data;
+    }
+})
